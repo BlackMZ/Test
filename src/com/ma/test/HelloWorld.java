c08efd44d1e03d24429550f3f217a8db093a4f2c
BlackMZ <3120002793@qq.com>
2017-03-22 18:35:15 +0800

    add comment

--- src/com/ma/test/HelloWorld.java
+++ src/com/ma/test/HelloWorld.java
@@ -1,10 +1,17 @@
 package com.ma.test;
-
+/**
+ * @ClassName: HelloWorld  
+ * @Description: this is a test file.  
+ * @author MZ  
+ * @2017年3月22日下午6:34:23
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello!");
+		
+		//
 	}
 
 }
